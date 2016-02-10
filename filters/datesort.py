12e1d22cb68fd23b2da50d373661f39e626d5ebe
Julian Doucette <julian@adblockplus.org>
2016-02-10 14:38:37 +0100

    Issue 3410 - Added date sort / format for press releases

--- filters/datesort.py
+++ filters/datesort.py
@@ -1,9 +1,2 @@
-import datetime, random
-
-def datesort(value, basedate="1970-01-01", column=0):
-  base = datetime.datetime.strptime(basedate, "%Y-%m-%d")
-  def randdiff(entry):
-    date = datetime.datetime.strptime(entry[column], "%Y-%m-%d")
-    diff = (date - base).days
-    return diff * 0.2 + random.randrange(diff) * 0.8
-  return sorted(value, key=randdiff)
+def datesort(collection):
+  return sorted(collection, reverse=True)
